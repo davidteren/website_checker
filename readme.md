a32830196ced5b512f875f066ff1ff0a79fc87e3
davidteren <drteren@gmail.com>
2023-11-22 14:47:35 +0200

    add readme

---
+++ readme.md
@@ -0,0 +1,21 @@
+# WebsiteChecker
+
+> _Calling this WebSiteChecker for lack of a better name. 🙄 
+
+### Backstory  
+
+I am eagerly awaiting the release of a new version of a synthesizer plugin I'm fond of, called VPS Avenger.
+
+Someone in the user group mentioned that videos likely showcasing new features and more have been uploaded to the company's YouTube channel that makes the synth.
+
+As you can see, the text is at the bottom of the Playlist.
+
+![img.png](img.png)
+
+#### Problem
+
+Selenium CSS selectors do not work on this YouTube page because it seems the content data is also loaded within a script tag, assigning a large JSON object to a JavaScript object that I assume is lazy-loaded. This would cause the Selenium selector to fail when attempting to select the element containing our text.
+
+### Solution
+
+Render the page and match the text we are checking to see if it has changed.
--- website_checker.rb
+++ website_checker.rb
@@ -26,15 +26,17 @@ module WebsiteChecker
 
         driver.navigate.refresh
         wait.until { driver.execute_script("return document.readyState") == "complete" }
-        current_state = driver.page_source.include?("4 unavailable videos are hidden")
+        current_state = driver.page_source.include?(TEXT_TO_LOOK_FOR)
 
         if current_state != previous_state
+          `say Change detected!` if mac_os?
           logger("Change detected for text: #{TEXT_TO_LOOK_FOR}... at URL: #{URL_TO_CHECK}", true)
           previous_state = current_state
         else
           logger("No changes detected for text: #{TEXT_TO_LOOK_FOR}... at URL: #{URL_TO_CHECK}")
         end
 
+        logger "Waiting for #{wait_human_time} before checking again..."
         sleep(300)
       end
     ensure
@@ -47,13 +49,6 @@ module WebsiteChecker
     puts message
     File.open(log_file_path, "a") { |file| file.puts(message) }
 
-    if change_detected && mac_os?
-      current_time = time_now
-      if current_time - $last_say_time > 300 #
-        `say Change detected!`
-        $last_say_time = current_time
-      end
-    end
   end
 
   def time_now
@@ -73,9 +68,17 @@ module WebsiteChecker
   end
 
   def wait_human_time
-    WAIT_TIME
+    format_duration(WAIT_TIME)
   end
 
+  def format_duration(seconds)
+    minutes = seconds / 60
+    remaining_seconds = seconds % 60
+    formatted_time = ""
+    formatted_time += "#{minutes} minute#{'s' unless minutes == 1} " if minutes > 0
+    formatted_time += "#{remaining_seconds} second#{'s' unless remaining_seconds == 1}" if remaining_seconds > 0
+    formatted_time.strip
+  end
 
   def mac_os?
     RUBY_PLATFORM =~ /darwin/
